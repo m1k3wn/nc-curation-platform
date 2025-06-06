d2505f66e049bac2837e37c7c79f9802856f4b45
m1k3wn <mike@mikewinnard.co.uk>
2025-06-06 12:57:47 +0100

    resolves header/footer overscroll

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,14 @@
 export default function Footer() {
   return (
     <footer className="bg-main border-t selection:py-2">
-      <div className="container mx-auto px-4 text-center text-body text-light text-inverse">
-        &copy; 2025 M1K3
+      <div className="container mx-auto px-4 text-center text-title text-sm text-light text-inverse">
+        <a
+          href="https://github.com/m1k3wn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          &copy; 2025 M1K3
+        </a>
       </div>
     </footer>
   );
