6140ec53a637c1050fcfd20c40608a565694b23a
m1k3wn <mike@mikewinnard.co.uk>
2025-06-06 13:51:58 +0100

    adds custom graphics

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
 import CollectionCascade from "../components/collections/CollectionCascade";
+// import landingGraphic from "../assets/cura-landing-graphic.png";
+import landingGraphic from "../assets/cura-landing-graphic-inv.png";
 
 export default function AboutPage() {
   return (
@@ -21,8 +23,16 @@ export default function AboutPage() {
               and images.
             </p>
           </section>
+          {/* Graphic */}
+          <div>
+            <img
+              src={landingGraphic}
+              alt="Cura About Graphic"
+              className="mx-auto mt-2 w-full max-w-2xl"
+            />
+          </div>
           {/* Links */}
-          <section className="text-center text-subtitle text-base max-w-2xl mx-auto bg-main text-inverse mt-4 px-2 py-4 rounded-lg">
+          <section className="text-center text-subtitle text-ml max-w-2xl mx-auto bg-main text-inverse mt-0 px-2 py-4 rounded-lg">
             <p>
               Built by
               <a
@@ -45,7 +55,7 @@ export default function AboutPage() {
                 Northcoders / Tech Returners.
               </a>
             </p>
-            <p className="text-subtitle text-base  text-inverse pt-4">
+            <p className="text-subtitle text-ml  text-inverse pt-4">
               <a
                 href="https://github.com/m1k3wn/nc-curation-platform"
                 target="_blank"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../components/search/SearchBar";
 
+import topGraphic from "../assets/cura-split-top.png";
+import btmGraphic from "../assets/cura-split-btm.png";
+
 export default function HomePage() {
   const navigate = useNavigate();
 
@@ -20,7 +23,7 @@ export default function HomePage() {
   };
 
   return (
-    <div className="py-12">
+    <div className="py-4">
       <div className="container mx-auto px-4">
         {/* Header */}
         <div className="text-center mb-4 mt-0">
@@ -33,8 +36,17 @@ export default function HomePage() {
         {/* Search Bar */}
         <SearchBar />
 
+        {/* Top Graphic  */}
+        <div>
+          <img
+            src={topGraphic}
+            alt="Cura Top Graphic"
+            className="mx-auto mt-4 w-full max-w-2xl"
+          />
+        </div>
+
         {/* Popular Searches */}
-        <div className="mt-8 text-center">
+        <div className="mt-0 text-center">
           <h2 className="text-subtitle mb-4">Try searching for...</h2>
           <div className="flex flex-wrap justify-center gap-2">
             {popularSearches.map((term) => (
@@ -48,6 +60,14 @@ export default function HomePage() {
               </button>
             ))}
           </div>
+        </div>
+        {/* Bottom Graphic */}
+        <div>
+          <img
+            src={btmGraphic}
+            alt="Cura Landing Graphic"
+            className="mx-auto mt-12 w-full max-w-2xl"
+          />
         </div>
       </div>
     </div>
